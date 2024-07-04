f4c152298f0b423bb481f32da27640152dcc0f51
R-nd-S <23rorman@backwellschool.net>
2024-07-04 17:41:47 +0000

    drop down box1 small text could do with an upgrade

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function testPrint(){
   console.log("TestPrint()")
 }
+const changeText = document.querySelector("#topBarText1");
+const dropDownBox = document.querySelector("#dropDown");
+dropDownBox.style.display = "none";
+
+changeText.addEventListener("click", function() {
+  if (changeText.textContent === "Contact-us ▶") {
+    changeText.textContent = "Contact-us ▼";
+    dropDownBox.style.display = "block";  // Show the dropdown
+  } else {
+    changeText.textContent = "Contact-us ▶";
+    dropDownBox.style.display = "none";  // Hide the dropdown
+  }
+});
