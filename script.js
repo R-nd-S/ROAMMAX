54d7be630ad4a125b9bd2402119ed68243d70ccd
R-nd-S <23rorman@backwellschool.net>
2024-07-05 15:04:24 +0000

    mainButton text gradient colour [color] (js set @ start)

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function testPrint(){
   console.log("TestPrint()")
 }
-const signedIn = "Sign up/in"
+const signedIn = "Start creating"
 const mainButton = document.querySelector("#mainTextButton");
 if(mainButton.textContent==signedIn){
   console.log("")
 }else{
    mainButton.textContent=signedIn;
+  mainButton.style.background = "linear-gradient(to right, rgba(139, 52, 46, 0.8), rgba(243, 171, 64, 0.8))";
+  mainButton.style.setProperty('-webkit-background-clip', 'text');
+  mainButton.style.setProperty('-webkit-text-fill-color', 'transparent');
+  mainButton.style.setProperty('background-clip', 'text');
+  mainButton.style.setProperty('text-fill-color', 'transparent');
 }
 const changeText = document.querySelector("#topBarText1");
 const dropDownBox = document.querySelector("#dropDown");
