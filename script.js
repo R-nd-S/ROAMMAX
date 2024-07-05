3c879c22f068c99331f3e5be5bd974d00b74c660
R-nd-S <23rorman@backwellschool.net>
2024-07-05 15:25:23 +0000

    original text fix ., tried to fix the white background but couldn't with way text was set up

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function testPrint(){
   console.log("TestPrint()")
 }
+function gradientFill() {
+  const mainButton = document.querySelector("#mainTextButton");
+  
+  mainButton.style.background = "linear-gradient(to right, rgba(139, 52, 46, 0.8), rgba(243, 171, 64, 0.8))";
+  mainButton.style.setProperty('-webkit-background-clip', 'text');
+  mainButton.style.setProperty('-webkit-text-fill-color', 'transparent');
+  mainButton.style.setProperty('background-clip', 'text');
+  mainButton.style.setProperty('text-fill-color', 'transparent');
+  mainButton.computedStyleMap.background = "rgb(255, 255, 255, 0.2)";
+}
+
 const params = new URLSearchParams(window.location.search); //parse params
 const p = params.get("mainButtonText"); //get p param
 console.log(p)
@@ -14,11 +25,7 @@ if(mainButton.textContent==signedIn){
   console.log("")
 }else{
    mainButton.textContent=signedIn;
-  mainButton.style.background = "linear-gradient(to right, rgba(139, 52, 46, 0.8), rgba(243, 171, 64, 0.8))";
-  mainButton.style.setProperty('-webkit-background-clip', 'text');
-  mainButton.style.setProperty('-webkit-text-fill-color', 'transparent');
-  mainButton.style.setProperty('background-clip', 'text');
-  mainButton.style.setProperty('text-fill-color', 'transparent');
+  gradientFill()
 }
 const changeText = document.querySelector("#topBarText1");
 const dropDownBox = document.querySelector("#dropDown");
@@ -40,3 +47,12 @@ dropDownBox.style.display = "none";
  function refresh(){
     window.location.href="https://r-nd-s.github.io/ROAMMAX"
  }
+mainButton.addEventListener('mouseover', () => {
+  mainButton.style.background = 'rgba(255, 255, 255, 0.4)';
+  gradientFill();
+});
+
+mainButton.addEventListener('mouseout', () => {
+  mainButton.style.background = 'rgba(255, 255, 255, 0.2)';
+  gradientFill();
+});
