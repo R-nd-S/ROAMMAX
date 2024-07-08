761bf710eed3abd4705877196d77efe1ea9e61e6
R-nd-S <23rorman@backwellschool.net>
2024-07-08 16:27:47 +0000

    js responsive mainText, mainTextButton (small-320, medium-375, big-425, tablet-768,  laptop-1024)[to-n-extent]-,..

--- script.js
+++ script.js
@@ -15,6 +15,45 @@ function gradientFill() {
   mainButtonText.style.setProperty('text-fill-color', 'transparent');
   mainButtonText.computedStyleMap.background = "rgb(255, 255, 255, 0.2)";
 }
+function adjustStyles(){
+  console.log('adjustStyles()')
+  if (window.outerWidth <= 932) {
+      const mainText = document.getElementById('mainText');
+      const mainTextButton = document.getElementById('mainTextButton');
+
+      if (window.outerWidth <= 374) {
+          mainText.style.setProperty('font-size', "56px");
+          mainTextButton.style.setProperty('font-size', "50px");
+          mainTextButton.style.setProperty('left', '15%');
+          mainText.style.setProperty('left', '2%');
+          mainText.style.setProperty('top', '44%');
+      } else if (window.outerWidth <= 424) {
+          mainText.style.setProperty('font-size', "58px");
+          mainTextButton.style.setProperty('font-size', "50px");
+          mainTextButton.style.setProperty('left', '15%');
+          mainText.style.setProperty('left', '8%');
+          mainText.style.setProperty('top', '44%');
+      } else if (window.outerWidth <= 474) {
+          mainText.style.setProperty('font-size', "64px");
+          mainTextButton.style.setProperty('font-size', "54px");
+          mainTextButton.style.setProperty('left', '19%');
+          mainText.style.setProperty('left', '10%');
+          mainText.style.setProperty('top', '42%');
+      } else if (window.outerWidth <= 769) {
+        mainText.style.setProperty('font-size', "74px");
+        mainTextButton.style.setProperty('font-size', "54px");
+        mainTextButton.style.setProperty('left', '24%');
+        mainText.style.setProperty('left', '14%');
+        mainText.style.setProperty('top', '40%');
+    } else{
+        mainText.style.setProperty('font-size', "800%");
+        mainTextButton.style.setProperty('font-size', "400%");
+        mainTextButton.style.setProperty('left', '14%');
+        mainText.style.setProperty('left', '14%');
+        mainText.style.setProperty('top', '34%');
+    }
+  }
+}
 
 const params = new URLSearchParams(window.location.search); //parse params
 const p = params.get("mainButtonText"); //get p param
@@ -22,6 +61,8 @@ const mainButtonText = document.querySelector("#mainTextButtonText");
 const mainButton = document.querySelector("#mainTextButton");
 const changeText = document.querySelector("#topBarText1");
 const dropDownBox = document.querySelector("#dropDown");
+const topBar = document.querySelector("#topBar");
+const topLeftLogo = document.querySelector("#topLeftLogo");
 if(p==null){
   var signedIn = "Sign-in/up";
 }else{
@@ -34,20 +75,17 @@ if(mainButtonText.textContent==signedIn){
   gradientFill()
 }
 
+if(window.innerWidth<=827){
+  topLeftLogo.src = "img/image-rsmidk (1) (1).png";
+  document.getElementById('topBarWhiteBackground').style.width = "50%";
+  document.getElementById('topBarWhiteBackground').style.right = "0%";  
+  document.getElementById('topBarText3').style.right = "4%";
+}
+
 dropDownBox.style.display = "none";
-
-// changeText.textContent = "Contact-us ▶";
-// dropDownBox.style.display = "none";  // Hide the dropdown
-
-// changeText.addEventListener("click", function() {
-//   if (changeText.textContent === "Contact-us ▶") {
-//     changeText.textContent = "Contact-us ▼";
-//     dropDownBox.style.display = "block";  // Show the dropdown
-//   } else {
-//     changeText.textContent = "Contact-us ▶";
-//     dropDownBox.style.display = "none";  // Hide the dropdown
-//   }
-// });
+adjustStyles();
+  
+window.addEventListener('resize', adjustStyles);
 
 mainButton.addEventListener('mouseover', () => {
   mainButton.style.background = 'rgba(255, 255, 255, 0.4)';
